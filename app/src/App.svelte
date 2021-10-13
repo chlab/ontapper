3daa24b3fb139d63366740964cecf553efca39fa
Christof Leuenberger <christof.leuenberger@gmail.com>
2021-10-13 14:15:38 +0200

    refactor taplist into it's own component

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,40 +1,23 @@
 <script>
+	import './Tailwind.svelte';
+	import Taplist from './components/Taplist.svelte';
+	import Skeleton from './components/Skeleton.svelte';
+
 	async function getTaplist() {
 		const response = await fetch('/api/taplist');
 		return response.json();
 	}
 </script>
 
-<main class="bg-black min-h-screen">
+<main class="min-h-screen">
 
-	<!-- <div class="mx-auto mt-20">
-		<div class="flex animate-pulse flex-row items-center justify-center space-x-5">
-			<div class="w-20 h-20">
-				<span class="text-big text-white md:text-huge align-top">10</span>
-			</div>
-			<div class="flex flex-col space-y-3">
-				<div class="w-36 bg-gray-300 h-6 rounded-md"></div>
-				<div class="w-24 bg-gray-300 h-6 rounded-md"></div>
-			</div>
-		</div>
-	</div> -->
-
-	<div class="container mx-auto divide-gray-300 divide-y-2 divide-solid">
+	<div class="container mx-auto">
 		{#await getTaplist()}
-			<p class="font-display text-2xl text-center pt-10 text-white">loading awesome taplist</p>
+			<Skeleton/>
+			<Skeleton/>
+			<Skeleton/>
 		{:then taplist}
-			{#each taplist.taplist as beer, tapNr}
-				<div class="flex flex-grow md:space-x-10 space-x-4 md:py-4 py-2">
-					<div class="w-16 min-w-16 md:text-right text-center">
-						<span class="text-big md:text-huge inline-block text-white font-display">{tapNr + 1}</span>
-					</div>
-					<div class="">
-						<h2 class="text-3xl text-white font-display uppercase">{beer.name}</h2>
-						<span class="block text-white font-display font-bold text-2xl">{beer.brewery}</span>
-						<span class="block text-white font-display text-xl">{beer.style} ({beer.abv}%)</span>
-					</div>
-				</div>
-			{/each}
+			<Taplist taplist="{taplist.taplist}" />
 		{:catch error}
 			<p class="font-display text-2xl text-center pt-10 text-red-400">couldn't load taplist :/</p>
 		{/await}
---
+++ app/src/components/Taplist.svelte
@@ -0,0 +1,28 @@
+<script>
+    export let taplist = [];
+</script>
+
+<div class="divide-gray-300 divide-y-2 divide-solid">
+    {#each taplist as beer, tapNr}
+        <div class="flex flex-grow md:space-x-10 space-x-4 md:py-4 py-2">
+            <div class="w-16 min-w-16 md:text-right text-center">
+                <span class="text-big md:text-huge inline-block tap-text">{tapNr + 1}</span>
+            </div>
+            <div class="">
+                <h2 class="text-3xl tap-text uppercase">
+                    {beer.name}
+                </h2>
+                    <button class="block tap-text font-bold text-2xl">{beer.brewery}</button>
+                <span class="block tap-text text-xl">
+                    {beer.style} ({beer.abv}%)
+                </span>
+            </div>
+        </div>
+    {/each}
+</div>
+
+<style lang="postcss">
+    .tap-text {
+        @apply text-white font-display;
+    }
+</style>

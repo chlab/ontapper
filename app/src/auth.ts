73d1e7e914f83bebc12698d294efda3b4b09bc3b
Christof Leuenberger <christof.leuenberger@gmail.com>
2021-10-26 22:51:33 +0200

    fix login bug

--- app/src/auth.ts
+++ app/src/auth.ts
@@ -1,17 +1,28 @@
-import { getAuth, signInAnonymously, onAuthStateChanged } from 'firebase/auth';
+import { Observable, filter } from 'rxjs';
+import { getAuth, signInAnonymously, signOut } from 'firebase/auth';
 import { authState } from 'rxfire/auth';
 
 import { app } from './firebase';
 
-export function initAuth() {
+export function initAuth(): Observable<User> {
     const auth = getAuth(app);
-    signInAnonymously(auth)
-        .catch((error) => {
-            console.error('Could not sign in anonymously', error.message, error.code);
-        });
+    try {
+        signInAnonymously(auth);
+    } catch (e) {
+        console.error('Could not sign in anonymously', e);
+    }
 
-    return authState(auth);
+    return authState(auth).pipe(
+        filter(user => !!user)
+    );
 }
+
+export async function logout() {
+    const auth = getAuth(app);
+    await signOut(auth);
+}
+// expose logout function for easier local testing
+globalThis.firebaseLogout = logout;
 
 export interface User {
     uid: string;
